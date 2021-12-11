c886d6e43748e98b5705f6cc8d16d35d55badab5
20181134 <91303066+20181134@users.noreply.github.com>
2021-12-11 21:21:01 +0900

    bug fixes

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@ create database login_db character set utf8 collate utf8_general_ci;
 grant all on login_db.* to 'admin'@'localhost' identified by 'password';
 use login;
 
-create table login (
+create table login_list (
     username varchar(30) not null,
     password varchar(30) not null
 )
